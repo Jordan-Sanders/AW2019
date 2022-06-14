59b9a8336b49b61e8c49c4292aab07a9c96587b7
Jordan-Sanders <83335543+Jordan-Sanders@users.noreply.github.com>
2022-06-14 13:21:50 +0300

    Update Person.AddressType.sql

--- Tables/Person.AddressType.sql
+++ Tables/Person.AddressType.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE [Person].[AddressType] (
+﻿CREATE TABLE [Person].[Address] (
   [AddressTypeID] [int] IDENTITY,
   [AddressName] [dbo].[Name] NOT NULL,
   [rowguid] [uniqueidentifier] NOT NULL CONSTRAINT [DF_AddressType_rowguid] DEFAULT (newid()) ROWGUIDCOL,
